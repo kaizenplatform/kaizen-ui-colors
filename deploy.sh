5c4e6459c68cbcf414ec49ad83f58be535fda87b
Yoshihide Jimbo <yjimbo@gmail.com>
2014-12-02 17:16:06 +0900

    Add color name

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,1 @@
-script_dir="$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")"; pwd)"
-lib_dir="${script_dir}/preview/lib"
-styles_dir="${script_dir}/preview/styles"
-sass --style expanded --sourcemap=none -r ${lib_dir}/sass-utilities.rb ${styles_dir}/style.scss ${styles_dir}/style.css
-
-git subtree push --prefix preview/ origin gh-pages
+git subtree push --prefix preview/ gh-pages master
