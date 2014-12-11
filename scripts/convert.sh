dffb444b85e22807110b4c1c3eb266a3a4c4af05
Atsushi Nagase <a@ngs.io>
2014-12-11 15:04:31 +0900

    Fix deploy script

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+set -e
 tools_dir="$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")"; pwd)"
 styles_dir="${tools_dir}/../preview/styles"
 sass --style compact --sourcemap=none -r ${tools_dir}/sass-utilities.rb ${styles_dir}/style.scss ${styles_dir}/style.css
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,12 @@
-git subtree push --prefix preview/ origin gh-pages
+#!/bin/bash
+
+set -e
+git clone -b gh-pages git@github.com:kaizenplatform/kaizen-ui-colors.git preview-gh-pages
+cd preview-gh-pages
+rm -rf *
+cp -R ../preview/* .
+git add -A
+git commit -am "Auto build preview on ${CIRCLE_BUILD_NUM}"
+git push origin gh-pages
+cd ../
+rm -rf preview-gh-pages
