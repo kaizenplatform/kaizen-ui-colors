cbb81dedb370f0b8caa7808d503a9229e79c38b2
laco0416 <laco0416+github@gmail.com>
2017-08-25 15:17:39 +0900

    Generate lib/colors.ts from colors.json

---
+++ scripts/generate.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const camelcase = require('camelcase');
+
+const getColorsJSON = () => {
+  return JSON.parse(
+    fs.readFileSync(
+      path.resolve(__dirname, '../kaizen-ui-colors.json'),
+      'utf-8'
+    )
+  );
+};
+
+const generateColors = (colorsJSON, outPath) => {
+  const formatted = Object.keys(colorsJSON.nameToHex).reduce((map, key) => {
+    const colorName = camelcase(key.replace('color-', ''));
+    map[colorName] = colorsJSON.nameToHex[key];
+    return map;
+  }, {});
+  fs.writeFileSync(
+    outPath,
+    '// DO NOT EDIT! This code is generated automatically.\n' +
+      `export const colors = ${JSON.stringify(formatted, null, 2)};`,
+    'utf-8'
+  );
+};
+
+const outPath = path.resolve(__dirname, '../src/colors.ts');
+const colorsJSON = getColorsJSON();
+console.log(`Generating colors.ts in "${outPath}" ...`);
+generateColors(colorsJSON, path.resolve(__dirname, '../src/colors.ts'));
+
+console.log(`Done!`);
